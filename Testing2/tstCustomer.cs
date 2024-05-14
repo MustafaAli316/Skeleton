2364046143110d098397766cc317da36e21ec2c8
James Clarke <P2595287@my365.dmu.ac.uk>
2024-05-14 17:24:58 +0100

    Tested first class

--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -17,13 +17,15 @@ namespace Testing2
         }
         [TestMethod]
         public void CustomerIdPropertyOK()
+        {
             //create an instance of the class we want to create
             clsCustomer AnCustomer = new clsCustomer();
-        //create some test data to assign to the property
-        Int32 TestData = 1;
-        //assign the data to the property
-        AnCustomer.CustomerId = TestData;
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnCustomer.CustomerId = TestData;
             //test to see the two values are same
             Assert.AreEqual(AnCustomer.CustomerId, TestData);
+        }
     }
 }
