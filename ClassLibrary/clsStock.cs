322af5fcf44d365319789dbd0871f3743d55a7f3
Mustafa Ali <P2840453@my365.dmu.ac.uk>
2024-05-14 01:52:01 +0100

    mo

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,11 +4,63 @@ namespace ClassLibrary
 {
     public class clsStock
     {
-        public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
 
-        public int ShoesNo { get; set; }
-        public string ShoesDesc { get; set; }
+        //private data member for the shoes no property
+        private int mShoesNo;
+        private DateTime mDateAdded;
+        private string mShoesDesc;
+
+        public bool Active { get; set; }
+        public DateTime DateAdded 
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDateAdded;
+            }
+
+
+            set
+            {
+                //this line of code sends data out of the property
+                mDateAdded = value;
+            }
+        }
+
+        //ShoesNo public property
+        public int ShoesNo {
+
+            get
+            {
+                //this line of code sends data out of the property
+                return mShoesNo;
+            }
+
+
+            set
+            {
+                //this line of code sends data out of the property
+                mShoesNo = value;
+            }
+
+
+        }
+
+        public string ShoesDesc 
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mShoesDesc;
+            }
+
+
+            set
+            {
+                //this line of code sends data out of the property
+                mShoesDesc = value;
+            }
+        }
         public string ShoesBrand { get; set; }
         public string ShoesColour { get; set; }
 
@@ -18,8 +70,10 @@ namespace ClassLibrary
 
         public bool Find(int shoeNo)
         {
-           // throw new NotImplementedException();
 
+            //set the private data members to the test data value
+            mShoesNo = 1;
+            mDateAdded = Convert.ToDateTime("23/12/2022");
             //always return true
             return true;
         }
