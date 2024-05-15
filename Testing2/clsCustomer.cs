4f5a1dc936882f0fc53182b3174bf93d2097f6bb
James Clarke <P2595287@my365.dmu.ac.uk>
2024-05-15 22:43:52 +0100

    all done

---
+++ Testing2/clsCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testing2
+{
+    public class clsCustomer
+    {
+        internal string EmailAddress;
+        internal DateTime DateOfBirth;
+        internal bool LoyalCustomer;
+
+        public int CustomerId { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Address { get; internal set; }
+    }
+}
