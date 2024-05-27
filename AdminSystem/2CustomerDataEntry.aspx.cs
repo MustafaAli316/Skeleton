61248cc344ac53a41ecd6e35442350d153d88716
abdurr3 <118371182+abdurr3@users.noreply.github.com>
2024-05-27 18:17:21 +0100

    Update 2CustomerDataEntry.aspx.cs

--- AdminSystem/2CustomerDataEntry.aspx.cs
+++ AdminSystem/2CustomerDataEntry.aspx.cs
@@ -8,7 +8,28 @@ using System.Web.UI.WebControls;
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
+    { 
+      
+        //create a new instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+        //capture the customer id
+        ACustomer.CustomerId  = Convert.ToInt32(txtCustomerId.Text);
+        //capture the customer first name
+        ACustomer.FirstName = txtFirstName.Text;
+        //capture the customer first name
+        ACustomer.LastName = txtLastName.Text;
+        //capture the customer email address
+        ACustomer.EmailAddress = txtEmailAddress.Text;
+        //capture the customer Address
+        ACustomer.Address = txtAddress.Text;
+        //capture the customer date of birth
+        ACustomer.DateOfBirth = Convert.ToDateTime(DateTime.Now);
+        //capture the customer is that a loyal customer
+        ACustomer.LoyalCustomer = chkLoyalCustomer.Checked;
+        //store the customer in the session object
+        Session["ACustomer"] = ACustomer;
+        //navigate to the view page 
+        Response.Redirect("2CustomerViewer.aspx");
 
     }
 }
