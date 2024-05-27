b22a4d920e8c2dae4f4d0dc5a6198e621d2f513b
abdurr3 <118371182+abdurr3@users.noreply.github.com>
2024-05-27 18:15:21 +0100

    Update 2CustomerViewer.aspx.cs

--- AdminSystem/2CustomerViewer.aspx.cs
+++ AdminSystem/2CustomerViewer.aspx.cs
@@ -8,7 +8,25 @@ using System.Web.UI.WebControls;
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
+    { 
+       //create a new instance of clsCustomer
+ clsCustomer ACustomer = new clsCustomer();
+ //get the data from the session object
+ ACustomer = (clsCustomer)Session["ACustomer"];
+ //display the customer id for this entry
+ Response.Write(ACustomer.CustomerId);
+ //display the first name for this entry
+ Response.Write(ACustomer.FirstName);
+ //display the last name for this entry
+ Response.Write(ACustomer.LastName);
+ //display the date of birth for this entry
+ Response.Write(ACustomer.DateOfBirth);
+ //display the address for this entry
+ Response.Write(ACustomer.Address);
+ //display the email address for this entry
+ Response.Write(ACustomer.EmailAddress);
+ //display the loyal customer for this entry
+ Response.Write(ACustomer.LoyalCustomer);
 
     }
 }
