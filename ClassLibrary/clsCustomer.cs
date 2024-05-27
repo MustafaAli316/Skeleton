fd3299352a9e2ac615622c7635272ec7ea56f7f8
abdurr3 <118371182+abdurr3@users.noreply.github.com>
2024-05-27 18:18:56 +0100

    Update clsCustomer.cs

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -2,6 +2,14 @@
 {
     public class ClsCustomer
     {
-        public int CustomerId { get; set; }
+        //private data member for customer id
+ private int mCustomerId;
+ public int CustomerId { get; set; }
+ public string FirstName { get; set; }
+ public string LastName { get; set; }
+ public string Address { get; set; }
+ public string EmailAddress { get; set; }
+ public DateTime DateOfBirth { get; set; }
+ public bool LoyalCustomer { get; set; }
     }
 }
